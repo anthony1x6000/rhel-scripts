3e4d94c33143f2ee3e79c11683baae3809a631d2
anthony1x6000 <33004321+anthony1x6000@users.noreply.github.com>
2026-04-16 21:19:49 -0400

    Update first-boot.sh

--- first-boot.sh
+++ first-boot.sh
@@ -10,24 +10,19 @@ LOG_FILE="/var/log/first-boot-test-zram.log"
 
 # vm.watermark_boost_factor = 0
 # "controls the level of reclaim when memory is being fragmented"
-# https://docs.kernel.org/admin-guide/sysctl/vm.html
-# Disabled this because it may stress out the CPU when the kernel keeps trying to shove stuff from regular ram into zram
-
 # vm.watermark_scale_factor
-# aggressiveness of kswapd. 
-# set higher so the kernel doesnt start trying to clean memory when it doesnt have to, reducing CPU 
+# aggressiveness of kswapd.
+# https://docs.kernel.org/admin-guide/sysctl/vm.html 
 
 # vm.page-cluster = 0
 # one page only 2^0=1, zram doesnt have seek time becuase it's ram 
 
-ZRAM_SIZE="2048" #mb
+ZRAM_SIZE="1" # 100% of ram, double ram size but not actually double
 ZRAM_COMPRESSION_ALGORITHM="zstd"
 ZRAM_SWAP_PRIORITY="100"
 ZRAM_FS_TYPE="swap"
 
 VM_SWAPPINESS="180"
-VM_WATERMARK_BOOST_FACTOR="0"
-VM_WATERMARK_SCALE_FACTOR="125"
 VM_PAGE_CLUSTER="0"
 
 # --- END ZRAM CONFIG VARS ---
@@ -67,7 +62,7 @@ dnf install -y zram-generator # redundant
 # note for myself: <<EOF could be <<LAPTOP or something similar, any word works 
 cat <<EOF > /etc/systemd/zram-generator.conf
 [zram0]
-zram-size = ${ZRAM_SIZE}
+zram-fraction = ${ZRAM_SIZE}
 compression-algorithm = ${ZRAM_COMPRESSION_ALGORITHM}
 swap-priority = ${ZRAM_SWAP_PRIORITY}
 fs-type = ${ZRAM_FS_TYPE}
@@ -75,15 +70,12 @@ EOF
 
 cat <<EOF > /etc/sysctl.d/99-zram.conf
 vm.swappiness = ${VM_SWAPPINESS}
-vm.watermark_boost_factor = ${VM_WATERMARK_BOOST_FACTOR}
-vm.watermark_scale_factor = ${VM_WATERMARK_SCALE_FACTOR}
 vm.page-cluster = ${VM_PAGE_CLUSTER}
 EOF
 
 sysctl --system # applies kernel params wo reboot 
 
 systemctl daemon-reload
-systemctl start /dev/zram0
 
 # https://developers.cloudflare.com/cloudflare-one/networks/connectors/cloudflare-tunnel/do-more-with-tunnels/local-management/create-local-tunnel/
 curl -fsSl https://pkg.cloudflare.com/cloudflared.repo | tee /etc/yum.repos.d/cloudflared.repo
